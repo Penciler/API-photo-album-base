848523abfa13602392613c312bdd0f952dae0d6f
Penciler <eraser41@livemail.tw>
2018-03-22 20:31:34 +0800

    add jbuilder for index

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,15 +1,6 @@
 class Api::V1::PhotosController < ApiController
   def index
     @photos = Photo.all
-    render json: {
-      data: @photos.map do |photo|
-        {
-          title: photo.title,
-          date: photo.date,
-          description: photo.description
-         }
-      end
-    }
   end
 
   def show
